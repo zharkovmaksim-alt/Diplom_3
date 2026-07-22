a95d933ce9b19242bbf1d252712214bfc33be324
Maksim <zharkov_maksim@mail.ru>
2026-07-23 02:44:18 +0300

    Исправлен RegisterPage: убран JavascriptExecutor

--- src/test/java/pageobject/RegisterPage.java
+++ src/test/java/pageobject/RegisterPage.java
@@ -2,9 +2,7 @@ package pageobject;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -54,9 +52,7 @@ public class RegisterPage {
 
     @Step("Клик на ссылку 'Войти'")
     public void clickLoginLink() {
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(loginLink));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].click();", element);
+        wait.until(ExpectedConditions.elementToBeClickable(loginLink)).click();
     }
 
     @Step("Регистрация пользователя: {email}")
